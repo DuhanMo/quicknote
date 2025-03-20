200d65742437b3d9f0c08d6ec36921da0994b4fb
Duhan Mo <duhandv@gmail.com>
2025-03-20 21:35:17 +0900

    refactor: remove unused variable

--- src/main/kotlin/io/duhanmo/quicknote/QuicknoteState.kt
+++ src/main/kotlin/io/duhanmo/quicknote/QuicknoteState.kt
@@ -8,13 +8,11 @@ import com.intellij.openapi.components.Storage
 @Service(Service.Level.PROJECT)
 @State(name = "QuicknoteState", storages = [Storage("quicknote.xml")])
 class QuicknoteState : PersistentStateComponent<QuicknoteState> {
-    private var notes: MutableList<Pair<String, String>> = mutableListOf()
     var draftContent: String = ""
 
     override fun getState(): QuicknoteState = this
 
     override fun loadState(state: QuicknoteState) {
-        this.notes = state.notes
         this.draftContent = state.draftContent
     }
 }
