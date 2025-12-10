9535d0bcc23db720224bc7b1cc9bf1d2d29f5f6b
Duhan Mo <duhandv@gmail.com>
2025-12-10 23:01:00 +0900

    feat: limit draft content length to improve performance

--- src/main/kotlin/io/duhanmo/quicknote/QuicknoteState.kt
+++ src/main/kotlin/io/duhanmo/quicknote/QuicknoteState.kt
@@ -5,10 +5,19 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 
+private const val MAX_CONTENT_LENGTH = 1_000_000
+
 @Service(Service.Level.PROJECT)
 @State(name = "QuicknoteState", storages = [Storage("quicknote.xml")])
 class QuicknoteState : PersistentStateComponent<QuicknoteState> {
     var draftContent: String = ""
+        set(value) {
+            field = if (value.length > MAX_CONTENT_LENGTH) {
+                value.substring(0, MAX_CONTENT_LENGTH)
+            } else {
+                value
+            }
+        }
 
     override fun getState(): QuicknoteState = this
 
